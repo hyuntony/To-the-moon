a1108fb714f9a73728f31c31a60cacdc06ae18da
dextergb <dextergb@rogers.com>
2021-07-31 23:03:42 -0400

    Add css and basic layout with background image

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Button } from "./Button";
 
-import './Intro.scss'
+import "./Intro.scss";
+
+const background = require("./public/images/test-image.jpeg");
 
 const Intro = () => {
   return (
-    <div>
-      Intro
+    <div className="intro-container">
+      <img src={background} alt="" />
+      <h1>To The Moon!</h1>
+      <p>Insert motto/mission/slogan here</p>
+      <div className="intro-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
