b0382ba157456362f329f27e0e246a4cc14add1c
Kevin Yu <kevin.yu.kfy@gmail.com>
2021-07-31 23:33:56 -0400

    Nav bar setup

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
-
-const Nav = () => {
-  return (
-    <div>
-      
+import SimpleMenu from './nav/SimpleMenu'
+import Logo from './Universal/Logo'
+import GetStartedButton from './nav/GetStartedButton'
+import TextButtons from './Universal/TextButtons'
+import './Nav.scss'
+const Nav = (props) => {
+  const loggedIn = props.loggedIn
+  if (loggedIn) {
+    return (
+<div className="Nav">
+      <Logo />
+      <SimpleMenu />
     </div>
-  )
+    )
+  } else {
+    return (
+      <div className="Nav">
+      <Logo />
+      <div className = 'right_nav'>
+      <TextButtons text="About Us" href='about'/>
+      <TextButtons text="Login" href='login'/>
+      <GetStartedButton />
+      </div>
+    </div>
+    )
+  }
 }
 
 export default Nav
