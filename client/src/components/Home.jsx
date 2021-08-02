1af8b3ccf660711b26f14b2a998866205826bbc6
dextergb <dextergb@rogers.com>
2021-08-02 17:43:08 -0400

    Basic search bar no functionality

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
+// import SearchBar from "components/SearchBar";
+
+import "./Home.scss";
 
 const Home = () => {
   return (
-    <div>
-      Hello Home
+    <div class="search">
+      <div class="fa fa-search"></div>
+      <input class="search-bar" type="text" placeholder="Search..." />
+      <div class="fa fa-times"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
