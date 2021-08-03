9ac46622e045f2b3707a10063651bf074ca2127e
dextergb <dextergb@rogers.com>
2021-08-02 21:49:34 -0400

    Added about us template

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,21 +1,72 @@
 import React from "react";
 import "./About.scss";
+import profile from "./profile.jpeg";
+
 const About = () => {
   return (
-    <div className="about">
-      <h1>What We Do</h1>
-      <p>
-        We provide you the starting point to get into learning how to invest. We
-        created this web app for you to be able to buy sell stocks and crypto
-        without actually using real money! We allow you to be able to practice
-        when to buy and when to sell, stimulating a real life stock exchange
-        application
-      </p>
-      <h1>Who We Are</h1>
-      <p>
-        To The Moon is backed by a team of amazing web developers! Our talented
-        team are ready to...{" "}
-      </p>
+    <div>
+      <div class="about-section">
+        <h1 class="about-us">About Us</h1>
+        <p>
+          We are a cutting edge, state of the art trading platform. Use our app
+          to get rich quick. To the moon baby!
+        </p>
+        <p>Also, we are fucking amazing. Read about us below.</p>
+      </div>
+
+      <h2 class="our-team">Our Team</h2>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <img class="profile" src={profile} alt="Logo" />
+            <div class="container">
+              <h2>Kevin Yu</h2>
+              <p class="title">Co-Founder</p>
+              <p class="describe">
+                Some text that describes me lorem ipsum ipsum lorem.
+              </p>
+              <p class="email">keviny@example.com</p>
+              <p>
+                <button class="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <img class="profile" src={profile} alt="Logo" />
+            <div class="container">
+              <h2>Anthony Kim</h2>
+              <p class="title">Co-Founder</p>
+              <p class="describe">
+                Some text that describes me lorem ipsum ipsum lorem.
+              </p>
+              <p class="email">anthonyk@example.com</p>
+              <p>
+                <button class="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <img class="profile" src={profile} alt="Logo" />
+            <div class="container">
+              <h2>Dexter GB</h2>
+              <p class="title">Co-Founder</p>
+              <p class="describe">
+                Some text that describes me lorem ipsum ipsum lorem.
+              </p>
+              <p class="email">dexterg@example.com</p>
+              <p>
+                <button class="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
