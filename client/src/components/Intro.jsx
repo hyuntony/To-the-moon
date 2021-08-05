3fe77fea10c04b281e4d2456f5fad1080b996fb4
Kevin Yu <kevin.yu.kfy@gmail.com>
2021-08-05 15:59:30 -0400

    added video

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -3,10 +3,14 @@ import { Button } from "./Button";
 
 import "./App.scss";
 import "./Intro.scss";
-
+import background from './intro/background.mp4'
 const Intro = () => {
   return (
     <div className="intro-container">
+      <video className='videoTag' autoPlay loop muted>
+    <source src={background} type='video/mp4' />
+</video>
+<div className='intro-details'>
       <h1>To The Moon!</h1>
       <p>Insert Slogan/Motto/Mission</p>
       <div className="intro-btns">
@@ -17,6 +21,7 @@ const Intro = () => {
         >
           GET STARTED
         </Button>
+      </div>
       </div>
     </div>
   );
