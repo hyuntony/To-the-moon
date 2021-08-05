39b38c0d0b0cf4c9d38974df7caf8efe0eeaa364
Kevin Yu <kevin.yu.kfy@gmail.com>
2021-08-05 17:02:08 -0400

    Updated navbar and intro page

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Button } from "./Button";
-
+import Video from './Video'
 import "./App.scss";
 import "./Intro.scss";
 import background from './intro/background.mp4'
+
 const Intro = () => {
   return (
     <div className="intro-container">
-      <video className='videoTag' autoPlay loop muted>
-    <source src={background} type='video/mp4' />
-</video>
-<div className='intro-details'>
-      <h1>To The Moon!</h1>
+      <Video />
+      <div className='overlay'>
+      <h1 contenteditable data-heading="Fracture">To The Moon!</h1>
       <p>Insert Slogan/Motto/Mission</p>
       <div className="intro-btns">
         <Button
@@ -21,7 +20,7 @@ const Intro = () => {
         >
           GET STARTED
         </Button>
-      </div>
+        </div>
       </div>
     </div>
   );
