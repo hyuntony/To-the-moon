5f59ab901a2be58fd8de7d3d8492bfdcc365504e
hyuntony <ankim90@gmail.com>
2021-08-06 19:18:09 -0400

    conflict resolved

--- client/src/components/details/DetailBuySell.jsx
+++ client/src/components/details/DetailBuySell.jsx
@@ -6,57 +6,22 @@ import axios from "axios";
 import Popup from "./Popup";
 
 const DetailBuySell = ({ user, price }) => {
-  const userId = user._id;
-  const [value, setValue] = useState(0);
-  const { symbol } = useParams();
-  const [added, setAdded] = useState(0);
-  const [isOpen, setIsOpen] = useState(false);
-  const [message, setMessage] = useState("");
-  const togglePopup = (e) => {
-    setIsOpen(!isOpen);
-    setMessage(e);
-  };
-  useEffect(() => {
-    axios.get(`/api/watchlist/${userId}/${symbol}`).then((res) => {
-      if (res.data !== null) {
-        setAdded(1);
-      }
-    });
-  }, []);
-  const addWatchlist = () => {
-    axios
-      .post(`/api/watchlist`, { symbol, user })
-      // .then((res) => {console.log(res.data)})
-      .then(() => {
-        togglePopup(`Watchlist Added`);
-      })
-      .then(() => {
-        setAdded(1);
-      });
-  };
-  const removeWatchlist = () => {
-    axios
-      .post(`/api/watchlist/delete`, { symbol, user })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .then(() => {
-        togglePopup(`Watchlist Removed`);
-      })
-      .then(() => {
-        setAdded(0);
-      });
-  };
-  const buyOnClick = () => {
-    if (value.length > 0 && value > 0) {
-      const action = "buy";
-      axios
-        .post(`/api/buy`, {
-          symbol: symbol,
-          quantity: value,
-          action: action,
-          user,
-          price: price,
+    const userId = user._id
+    const [value, setValue] = useState(0)
+    const { symbol } = useParams();
+    const [added, setAdded] = useState(0)
+    const [isOpen, setIsOpen] = useState(false);
+ const [message, setMessage] = useState('')
+    const togglePopup = (e) => {
+      setIsOpen(!isOpen);
+      setMessage(e)
+    }
+    useEffect(() => {
+        axios.get(`/api/watchlist/${userId}/${symbol}`)
+        .then((res)=>{
+            if (res.data !== null) {
+                setAdded(1)
+            }
         })
         }, [])
     const addWatchlist = () => {
@@ -90,79 +55,44 @@ const DetailBuySell = ({ user, price }) => {
             // .then(() => console.log(`Sell: ${value} ${symbol} Stock!`))
             }
     }
-  };
 
-  const whichButton = () => {
+const whichButton = () => {
     if (added === 0) {
-      return (
-        <Button
-          variant="outlined"
-          color="primary"
-          value={value}
-          onClick={addWatchlist}
-        >
-          Add To Watchlist
-        </Button>
-      );
+        return (<Button variant="outlined" color="primary" value={value} onClick={addWatchlist}>
+        Add To Watchlist
+    </Button>)
     } else {
-      return (
-        <Button
-          variant="outlined"
-          color="primary"
-          value={value}
-          onClick={removeWatchlist}
-        >
-          Remove From Watchlist
-        </Button>
-      );
+        return (<Button variant="outlined" color="primary" value={value} onClick={removeWatchlist}>
+        Remove From Watchlist
+    </Button>)
     }
-  };
-  return (
-    <div className="buy-sell-func">
-      <div className="details-buttons">
-        <TextField
-          id="outlined-basic"
-          type="text"
-          label="Quantity"
-          variant="outlined"
-          onChange={(event) => setValue(event.target.value)}
-        />
+}
+    return (
+        <div className='buy-sell-func'>
+            <div className='details-buttons'>
+
+      <TextField id="outlined-basic" type='text' label="Quantity" variant="outlined" onChange={event => setValue(event.target.value)}/>
       </div>
-      <div className="details-buttons">
-        <div>
-          {isOpen && (
-            <Popup
-              content={
-                <>
-                  <p>{message}</p>
-                  <button
-                    onClick={() => {
-                      setIsOpen(false);
-                    }}
-                  >
-                    Confirm
-                  </button>
-                </>
-              }
-              handleClose={togglePopup}
-            />
-          )}
+      <div className='details-buttons'>
+      <div>
+    {isOpen && <Popup
+      content={<>
+        <p>{message}</p>
+        <button onClick={()=> {setIsOpen(false)}}>Confirm</button>
+      </>}
+      handleClose={togglePopup}
+    />}
+  </div>
+      {whichButton()}
+            <Button variant="outlined" color="primary" value={value} onClick={buyOnClick}>
+                Buy
+            </Button>
+            <Button variant="outlined" color="primary" onClick={sellOnClick}>
+                Sell
+            </Button>
+            </div>
         </div>
-        {whichButton()}
-        <Button
-          variant="outlined"
-          color="primary"
-          value={value}
-          onClick={buyOnClick}
-        >
-          Buy
-        </Button>
-        <Button variant="outlined" color="primary" onClick={sellOnClick}>
-          Sell
-        </Button>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
 export default DetailBuySell;
