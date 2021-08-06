1964f7951ea85d40d8ef69f052bd6dbf6473f7c0
dextergb <dextergb@rogers.com>
2021-08-06 19:11:07 -0400

    Correct the watchlist

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-// import SearchBar from "components/SearchBar";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import Watchlist from "./Watchlist";
 import Search from "./search/Search";
 import HoldingRow from "./HoldingRow";
@@ -10,12 +12,31 @@ import Holdings from "./Holdings";
 
 import "./Home.scss";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const Home = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <Search />
-      <AccountInfo />
-      <Holdings />
+      <Grid container spacing={3}>
+        <Grid item xs={10}>
+          <AccountInfo />
+          <Holdings />
+        </Grid>
+        <Grid item xs={2}>
+          <Watchlist />
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- client/src/components/Watchlist.jsx
+++ client/src/components/Watchlist.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 import "./Watchlist.scss";
 
 const Watchlist = () => {
   return (
-    <div className="search">
-      <div className="fa fa-search"></div>
-      <input className="search-bar" type="text" placeholder="Search..." />
-      <div className="fa fa-times"></div>
+    <div>
+      <h4 className="watchlist-title">Watchlist</h4>
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid>
+          <Paper>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper>xs</Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper>xs</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
