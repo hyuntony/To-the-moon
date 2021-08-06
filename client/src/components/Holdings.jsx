e9bb653037b8157b665554e50850f1cca0c46b68
dextergb <dextergb@rogers.com>
2021-08-06 01:23:28 -0400

    Set up Balance and Holding Tables

---
+++ client/src/components/Holdings.jsx
@@ -0,0 +1,119 @@
+import * as React from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import { makeStyles } from "@material-ui/core/styles";
+import "./Holdings.scss";
+
+const columns = [
+  { field: "id" },
+  { field: "Name", width: 120 },
+  {
+    field: "Price",
+    type: "number",
+    width: 120,
+    valueFormatter: ({ value }) => `$${value}`,
+  },
+  {
+    field: "Shares",
+    type: "number",
+    width: 120,
+    valueFormatter: ({ value }) => `${value}`,
+  },
+  {
+    field: "buySell",
+    headerName: "Bought/Sold",
+    width: 160,
+    valueFormatter: ({ value }) => `${value}`,
+  },
+  {
+    field: "Status",
+    width: 120,
+    valueFormatter: ({ value }) => `${value}`,
+  },
+  {
+    field: "Date",
+    type: "number",
+    width: 120,
+    valueFormatter: ({ value }) => `${value}`,
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    Name: "AAPL",
+    Price: 7.1,
+    Status: "Pending",
+    Shares: -10,
+    Date: "Jan 1, 2021",
+    buySell: "Bought",
+  },
+  {
+    id: 2,
+    Name: "GOOGL",
+    Price: 14.9,
+    Status: "Filled",
+    Shares: 18.2,
+    Date: "Jan 1, 2021",
+    buySell: "Sold",
+  },
+  {
+    id: 3,
+    Name: "MSFT",
+    Price: 8.1,
+    Status: "Pending",
+    Shares: 12.3,
+    Date: "Jan 1, 2021",
+    buySell: "Sold",
+  },
+  {
+    id: 4,
+    Name: "TSLA",
+    Price: 20.2,
+    Status: "Filled",
+    Shares: 19.2,
+    Date: "Jan 1, 2021",
+    buySell: "Bought",
+  },
+];
+
+const useStyles = makeStyles({
+  root: {
+    "& .green": {
+      backgroundColor: "#00e676",
+      color: "#1a3e72",
+    },
+    "& .red": {
+      backgroundColor: "#ff1744",
+      color: "#1a3e72",
+    },
+    "& .yellow": {
+      backgroundColor: "#ffea00",
+      color: "#1a3e72",
+    },
+  },
+});
+
+export default function StylingAllCells() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h4 className="holdings-title">Holdings</h4>
+      <div style={{ height: 400, width: "100%" }} className={classes.root}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          getCellClassName={(params) => {
+            if (params.field === "id") {
+              return "";
+            }
+            if (params.value === "Pending") {
+              return "yellow";
+            }
+            return params.value === "Sold" ? "red" : "green";
+          }}
+        />
+      </div>
+    </div>
+  );
+}
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,6 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Watchlist from "./Watchlist";
 import Search from "./search/Search";
 import HoldingRow from "./HoldingRow";
+import AccountInfo from "./AccountInfo";
+import Holdings from "./Holdings";
 
 import "./Home.scss";
 
@@ -12,37 +14,8 @@ const Home = () => {
   return (
     <div>
       <Search />
-      <div>
-        <h1 className="holding-title">Holdings</h1>
-      </div>
-      <div className="container">
-        <table className="table">
-          <thead>
-            <tr className="table-headers">
-              <th>Ticker</th>
-              <th>Price</th>
-              <th>Date</th>
-              <th>Time</th>
-              <th>Shares</th>
-            </tr>
-          </thead>
-          <tbody>
-            <HoldingRow ticker="AAPL" />
-            <HoldingRow ticker="BRK.A" />
-            <HoldingRow ticker="AMZN" />
-            <HoldingRow ticker="GOOGL" />
-          </tbody>
-        </table>
-        {/* <div className="watchlist">
-            <table className="table mt-5">
-              <thead>
-                <tr>
-                  <th>Watchlist</th>
-                </tr>
-              </thead>
-            </table>
-          </div> */}
-      </div>
+      <AccountInfo />
+      <Holdings />
     </div>
   );
 };
