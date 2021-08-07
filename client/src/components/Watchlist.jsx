29e778f03e04567d588353b723e7eb06b6eb838f
dextergb <dextergb@rogers.com>
2021-08-06 20:25:09 -0400

    Material ui watchlist

--- client/src/components/Watchlist.jsx
+++ client/src/components/Watchlist.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import ReactDOM from "react-dom";
+import Table from "@material-ui/core/Table/Table";
+import TableHead from "@material-ui/core/TableHead/TableHead";
+import TableRow from "@material-ui/core/TableRow/TableRow";
+import TableCell from "@material-ui/core/TableCell/TableCell";
+import TableBody from "@material-ui/core/TableBody/TableBody";
+import Paper from "@material-ui/core/Paper/Paper";
+import Grid from "@material-ui/core/Grid/Grid";
 
 import "./Watchlist.scss";
 
@@ -8,20 +14,37 @@ const Watchlist = () => {
   return (
     <div>
       <h4 className="watchlist-title">Watchlist</h4>
-      <Grid
-        container
-        direction="column"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Grid>
-          <Paper>xs</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper>xs</Paper>
-        </Grid>
-        <Grid item xs>
-          <Paper>xs</Paper>
+      <Grid container justify={"center"}>
+        <Grid item xs={10} md={10} className={"flex-col-scroll"}>
+          <Paper style={{ overflowX: "auto" }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Content</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>Content</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Content</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Content</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Content</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Content</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Content</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Paper>
         </Grid>
       </Grid>
     </div>
